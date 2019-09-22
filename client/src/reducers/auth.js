6a92a0a14018f5e9fa9268f46ce8cb22a776f653
akashgiricse <akashgiricse@gmail.com>
2019-09-22 12:50:47 +0530

    Add login and logout actions, reducers in component

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,10 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 import { stat } from 'fs';
 
@@ -24,6 +27,7 @@ export default function(state = initialState, action) {
         user: payload
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -32,7 +36,9 @@ export default function(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
     case AUTH_ERROR:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
